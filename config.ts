8b0b9baea4835afccb5ee02f98a38f74df7c3f56
Akshay <akshay@bpract.com>
2023-10-31 13:40:21 +0530

    removed stuffs

--- config.ts
+++ config.ts
@@ -1,6 +1,2 @@
 import { readFileSync } from 'fs';
-import path from 'path';
-export const PRIVATE_KEY = readFileSync(
-  path.join(__dirname, '../keypair.pem'),
-  'utf8',
-);
+export const PRIVATE_KEY = readFileSync('./keypair.pem', 'utf8');
